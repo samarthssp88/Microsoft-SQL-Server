c83a5e31d56c291d0d1e50842bded8c7e60d59d9
Samarth S Patil <122732938+samarthssp88@users.noreply.github.com>
2023-07-06 16:28:51 +0530

    Delete SQL - TEST 3.sql

--- SQL - TEST 3.sql
+++
@@ -1,9 +0,0 @@
---> select top 5 address details where AddressLine2 is not null and AddressID is in descending order
-select * from Person.Address
-select top 5 * from Person.Address
-where AddressLine2 is not null
-order by AddressID desc
-
---> Move user databases using Detach and Attach technique
-
-
